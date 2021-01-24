c7fc0362b44e9cbf50ffffe01fb7e905a28244d5
NarminGale <narmingale@gmail.com>
2021-01-24 21:40:39 +0400

    Give the color changes to dots

--- Slideshow/script.js
+++ Slideshow/script.js
@@ -4,6 +4,7 @@ const right = document.querySelector('.right')
 const slides = document.querySelectorAll('.slide')
 const dots = document.querySelectorAll('.dot')
 
+dots[0].classList.add('active')
 slides.forEach(function (slide, index) {
   slide.style.left = `${index * 100}%`
 })
@@ -12,11 +13,23 @@ counter = 0
 left.addEventListener('click', function () {
   counter--
   carousel()
+  dotColor()
 })
 right.addEventListener('click', function () {
   counter++
   carousel()
+  dotColor()
 })
+
+function dotColor() {
+  dots.forEach(function (dot, index) {
+    if (index === counter) {
+      dot.classList.add('active')
+    } else {
+      dot.classList.remove('active')
+    }
+  })
+}
 
 function carousel() {
   if (counter == slides.length) {
