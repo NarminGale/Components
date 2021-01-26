084b4ce8147ae0731e0aca33a33fbdafc5ee4afa
NarminGale <narmingale@gmail.com>
2021-01-26 23:56:46 +0400

    Modified some codes

--- Slideshow-Gallery/script.js
+++ Slideshow-Gallery/script.js
@@ -2,3 +2,29 @@
 const left = document.querySelector('.left')
 const right = document.querySelector('.right')
 const slides = document.querySelectorAll('.slide')
+
+slides.forEach(function (slide, index) {
+  slide.style.left = `${index * 100}%`
+})
+
+let counter = 0
+left.addEventListener('click', function () {
+  counter--
+  carousel()
+})
+right.addEventListener('click', function () {
+  counter++
+  carousel()
+})
+
+function carousel() {
+  if (counter == slides.length) {
+    counter = 0
+  }
+  if (counter < 0) {
+    counter = slides.length - 1
+  }
+  slides.forEach(function (slide) {
+    slide.style.transform = `translateX(-${counter * 100}%)`
+  })
+}
