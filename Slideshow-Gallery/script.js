d918d0754aca65dcafdd69b0faea5f694e35f503
NarminGale <narmingale@gmail.com>
2021-01-28 22:38:40 +0400

    Gived opacity to image when it clicked

--- Slideshow-Gallery/script.js
+++ Slideshow-Gallery/script.js
@@ -2,23 +2,44 @@
 const left = document.querySelector('.left')
 const right = document.querySelector('.right')
 const slides = document.querySelectorAll('.slide')
+const images = document.querySelectorAll('.image img')
 
+images[0].classList.remove('demo')
 slides.forEach(function (slide, index) {
   slide.style.left = `${index * 100}%`
 })
 
-let counter = 0
+counter = 0
 left.addEventListener('click', function () {
   counter--
   carousel()
+  imageOverlay()
 })
 right.addEventListener('click', function () {
   counter++
   carousel()
+  imageOverlay()
+})
+
+function imageOverlay() {
+  images.forEach(function (image, index) {
+    if (index === counter) {
+      image.classList.remove('demo')
+    } else {
+      image.classList.add('demo')
+    }
+  })
+}
+
+images.forEach(function (image, index) {
+  image.addEventListener('click', function () {
+    counter = index
+    carousel()
+  })
 })
 
 function carousel() {
-  if (counter == slides.length) {
+  if (counter > slides.length - 1) {
     counter = 0
   }
   if (counter < 0) {
@@ -26,5 +47,12 @@ function carousel() {
   }
   slides.forEach(function (slide) {
     slide.style.transform = `translateX(-${counter * 100}%)`
+  })
+  images.forEach(function (image, index) {
+    if (index === counter) {
+      image.classList.remove('demo')
+    } else {
+      image.classList.add('demo')
+    }
   })
 }
